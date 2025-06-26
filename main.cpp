cf86c005926801e97ebe15702571e2bbf61f3bf2
ubutun-hao <leepi6364@gmail.com>
2025-06-26 22:15:44 +0800

    实现sort函数

--- main.cpp
+++ main.cpp
@@ -2,6 +2,19 @@
 
 void sort (int arr[], int size)
 {
+	int tmp = 0;
+	for (int i = 0; i < size - 1; i++)
+	{
+		for (int j = 0; j < size - i; ++j)
+		{
+			if (arr[j] > arr[j+1])
+			{	
+				 tmp = arr[j];
+				 arr[j] = arr[j+1];
+				 arr[j+1] = tmp;
+			}
+		}
+	}
 
 }
 
