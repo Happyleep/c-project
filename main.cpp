ecda19bdfc8380175f024cdf9b8379712e83dad8
win-hao <leepi6364@gmail.com>
2025-06-26 21:20:35 +0800

    创建main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void sort (int arr[], int size)
+{
+
+}
+
+int main ()
+{
+    int arr[] = {12, 4, 1, 54, 22};
+    int size = sizeof(arr) /sizeof(arr[0]);
+    sort(arr, size);
+
+    return 0;
+}
