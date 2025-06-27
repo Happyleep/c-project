d1f78711abaa60b97035ec10e749b070aeca2ff4
ubutun-hao <leepi6364@gmail.com>
2025-06-27 17:28:23 +0800

    modify sort-function

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 void sort (int arr[], int size)
 {
 	int tmp = 0;
-	for (int i = 0; i < size - 1; i++)
+	for (int i = 0; i < size; i++)
 	{
-		for (int j = 0; j < size - i; ++j)
+		for (int j = 0; j < i; ++j)
 		{
 			if (arr[j] > arr[j+1])
 			{	
@@ -23,6 +23,10 @@ int main ()
     int arr[] = {12, 4, 1, 54, 22};
     int size = sizeof(arr) /sizeof(arr[0]);
     sort(arr, size);
-
+    for (int i = 0; i < size; ++i)
+    {
+	std::cout << arr[i] << " " ;
+    }
+    std::cout << std::endl;
     return 0;
 }
